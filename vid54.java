c98eed6ffb5199973357208f84929c299c52f631
MayankAsutkar <mayankasutkar17102006@gmail.com>
2025-01-28 18:43:27 +0530

    vid54

---
+++ vid54.java
@@ -0,0 +1,26 @@
+class Calc
+{
+    public int add(int n1,int n2)
+    {
+        return n1 + n2;
+    }
+}
+class AdvCalc extends Calc
+{
+    public int add(int n1,int n2)
+    {
+        return n1 + n2 + 1;
+    }
+}
+public class vid54 {
+    public static void main(String arg[])
+    {
+        //suppose there is a method having same name in both super class and sub class.
+        //then in that case the method present in sub class is called overiding super class.
+        //this is known as METHOD OVERRIDING.
+        AdvCalc obj = new AdvCalc();
+        int r = obj.add(1,2);
+        System.out.println(r);
+    }
+
+}
